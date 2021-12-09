84f51b556f50d00ff2b55f16a6d598563a7ef191
Dragan Pepic <nedimco2006@gmail.com>
2021-12-09 17:52:34 +0100

    Add project files.

---
+++ Server/OsobaHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Teeest2.Shared;
+
+namespace Teeest2.Server
+{
+    public class OsobaHub : Hub
+    {
+        public void Metoda(Osoba o)
+        {
+            Console.WriteLine($"{o.Name} {o.Surname}");
+            Console.WriteLine("Dobri momci");
+        }
+    }
+}
